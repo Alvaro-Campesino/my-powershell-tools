4ff935a75980a9dbe59e19ec91e686f69128c296
Alvaro Campesino <57360851+Alvaro-Campesino@users.noreply.github.com>
2024-05-16 12:29:37 +0200

    Powershell scripts (#1)

---
+++ Check_ADUserPasswd.ps1
@@ -0,0 +1,24 @@
+﻿function Test-ADCredentials {
+    param (
+        [string]$username,
+        [string]$password,
+        [string]$domain
+    )
+
+    Add-Type -AssemblyName System.DirectoryServices.AccountManagement
+    $principalContext = New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::Domain, $domain)
+
+    try {
+        $isValid = $principalContext.ValidateCredentials($username, $password)
+        if ($isValid) {
+            Write-Output "Credentials are valid."
+        } else {
+            Write-Output "Credentials are not valid."
+        }
+    } catch {
+        Write-Error "An error occurred: $_"
+    }
+}
+
+# Usage
+Test-ADCredentials -username "username" -password "password" -domain "domain.intra.corp"
---
+++ Check_ADgroups.ps1
@@ -0,0 +1,22 @@
+﻿# Define the list of users
+$upns = "user@domain.com"
+$match = ""
+
+
+# Loop through each user
+foreach ($upn in $upns) {
+    # Get the user
+    $user = Get-ADUser -Filter "UserPrincipalName -eq '$upn'"
+
+    # Get the groups the user is a member of
+    $groups = Get-ADPrincipalGroupMembership -Identity $user | Select-Object -ExpandProperty Name
+   if ($match) {
+    $groups= $groups | Where-Object { $_ -like "*$match*" }
+   } 
+
+   $groups = $groups -join "`n    "
+    # Output the user and their groups
+    Write-Output "User: $upn"
+    Write-Output "Groups: |-
+    $groups"
+}
